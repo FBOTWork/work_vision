b4ba681e2265943958d1aca25572f48a1b2f87bd
Gabrieltlt <gabrieltlt721@gmail.com>
2025-06-26 16:28:49 -0300

    added .gitignore and update package.xml

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-# Este setup.py declara os módulos pertencentes ao pacote work_vision.
-# Se você tiver código python em "src/work_vision", ele será encontrado.
+# fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['work_vision'],
     package_dir={'': 'src'},
